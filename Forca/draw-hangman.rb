3d91eec966291cdcfcb5d6c3eec282868ce18aed
Isaac <isaac.kennedy@compasso.com.br>
2022-02-25 01:15:05 -0300

    feat: draw hang

--- Forca/draw-hangman.rb
+++ Forca/draw-hangman.rb
@@ -1,28 +1,51 @@
 def draw_hangman(lives)
+  # draws upper part
   puts('------------')
+  spaces_number = 5
   if (lives < 6 && lives > 0)
-    print("    O   ")
+    spaces_number = 0
+    print("    O")
   end
   if (lives < 1)
-    print("    Ø   ")
+    spaces_number = 0
+    print("    Ø")
   end
-  puts('')
+  print(" " * spaces_number)
+  puts('‾‾‾‾‾‾|')
+
+  # draws body / middle part
+  spaces_number = 11
   if (lives < 5)
     print("   /")
+    spaces_number = 7
   end
   if (lives < 4)
     print("|")
+    spaces_number = 6
   end
   if (lives < 3)
     print("\\")
+    spaces_number = 5
   end
+  spaces = " " * spaces_number
+  print(spaces + "|")
   puts('')
+
+  # draws bottom parts
+  spaces_number = 11
   if (lives < 2)
     print("   /")
+    spaces_number = 7
   end
   if (lives < 1)
+    spaces_number = 5
     print(" \\")
   end
-  puts('')
+  
+  spaces = " " * spaces_number
+  puts(spaces + "|")
+  spaces = " " * 10
+  puts(spaces + "/|\\")
+
   puts('------------')
 end
